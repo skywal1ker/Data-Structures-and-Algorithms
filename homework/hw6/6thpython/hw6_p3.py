f202f97475325477d7444f1ac3efcb79d3c9d4de
skywal1ker <ik11@bu.edu>
2022-10-07 00:00:00 +0600

    Homework

--- homework/hw6/6thpython/hw6_p3.py
+++ homework/hw6/6thpython/hw6_p3.py
@@ -2,8 +2,6 @@ import array_stack #For imoprting stacks
 
 
 buffer = array_stack.ArrayStack() #to create empty stack
-    
-
 
 for line in open("hw6_sentence.txt"): #for open input file 
     for word in line:                 #to get access to each word step by step
